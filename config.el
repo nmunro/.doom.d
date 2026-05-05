e6ba228f0aa8f386dce66624d4c900f723d01daf
NMunro <neilmunro@gmail.com>
2026-05-05 11:22:48 +0100

    Updated config

--- config.el
+++ config.el
@@ -1,32 +1,106 @@
-;;; .doom.d/config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark))
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;; (setq doom-theme 'doom-one)
 
 (setq doom-theme 'doom-solarized-dark)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `with-eval-after-load' block, otherwise Doom's defaults may override your
+;; settings. E.g.
+;;
+;;   (with-eval-after-load 'PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look them up).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
 (define-key key-translation-map (kbd "M-3") (kbd "#"))
 (define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
 (define-key evil-visual-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
 (define-key evil-normal-state-map (kbd "C-s") 'evil-numbers/dec-at-pt)
 (define-key evil-visual-state-map (kbd "C-s") 'evil-numbers/dec-at-pt)
 
-; Projectile
-(setq projectile-project-search-path '("~/quicklisp/local-projects/" "~/dev/" "~/Projects/"))
+(map! "C-z" #'suspend-frame)
+(map! :n "C-z" #'suspend-frame
+        :i "C-z" #'suspend-frame
+        :v "C-z" #'suspend-frame
+        :m "C-z" #'suspend-frame)
 
-; Fonts
-(setq doom-font (font-spec :family "Fira Code" :size 16 :weight 'Regular))
+(dolist (bin-path (list (expand-file-name "~/.local/bin")
+                        (expand-file-name "~/.roswell/bin")))
+    (add-to-list 'exec-path bin-path)
+    (setenv "PATH" (concat bin-path ":" (getenv "PATH"))))
 
-(after! lsp-mode
-  (lsp-register-client
-    (make-lsp-client :new-connection (lsp-stdio-connection "python3"
-                                      (lambda () '("localhost" "5678")))
-                     :major-modes '(python-mode)
-                     :server-id 'pylsp-docker)))
+(after! sly
+    (setq inferior-lisp-program "ros -Q run")
 
-(after! lsp-mode
-  (setq lsp-diagnostics-provider :auto  ; Use lsp-mode for diagnostics
-        lsp-pylsp-plugins-mypy-enabled t  ; Ensure mypy is enabled
-        lsp-pylsp-plugins-mypy-live-mode nil  ; Disable live mode if it's too performance-intensive
-        lsp-pylsp-plugins-mypy-args '("--ignore-missing-imports")))  ; Example of adding extra mypy args
+    (defun my/inferior-lisp-program ()
+      "Return the appropriate inferior Lisp program based on project context.
+
+      Uses qlot if a qlfile is found in the project, otherwise falls back to ros."
+      (if (locate-dominating-file default-directory "qlfile")
+          "qlot exec ros -Q run"
+          "ros -Q run"))
+
+    (add-hook 'lisp-mode-hook (lambda () (unless (sly-connected-p) (save-excursion (sly (my/inferior-lisp-program)))))))
